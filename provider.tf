73904e71f9cc179ffc8afb4e5382f97e179fcb3c
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-04-14 17:15:43 +0200

    name for VPC

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 # Creating VPC
 resource "aws_vpc" "wordpress-vpc" {
   cidr_block = "10.0.0.0/16"
-tags = {
-    Name = "wordpress-vpc"
+    tags = {
+        Name = "wordpress-vpc"
   }
 }
