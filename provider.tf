5e3595d8f0ff570bf9cfe370609ffadc884ace18
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-04-15 16:21:58 +0200

    creating subnet

--- provider.tf
+++ provider.tf
@@ -4,10 +4,12 @@ provider "aws" {
 # Creating VPC
 resource "aws_vpc" "wordpress-vpc" {
   cidr_block = "10.0.0.0/16"
+
     tags = {
       Name = "wordpress-vpc"
     }
 }
+
 # Creating Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.wordpress.id
