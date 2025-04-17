7daba20402e5f7a67f6862b69b36b49b11e1d5bd
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-04-17 11:54:29 +0200

    Creating local db and wordpress

--- provider.tf
+++ provider.tf
@@ -96,7 +96,7 @@ resource "aws_instance" "wordpress_server" {
   key_name      = "vockey"
   subnet_id     = aws_subnet.public_subnet.id
   vpc_security_group_ids = [
-    aws_security_group.web-security-group.i
+    aws_security_group.web-security-group.id
   ]
 
   user_data = <<-EOF
