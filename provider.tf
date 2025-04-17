233ecb579a7172aa84d9f069b6f9aa941d5a97e5
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-04-17 12:16:14 +0200

    Creating EC2 instance local db and wordpress

--- provider.tf
+++ provider.tf
@@ -83,6 +83,91 @@ resource "aws_instance" "web_server" {
   ]
 
   associate_public_ip_address = true
+  provider "aws" {
+  region = "us-west-2"
+}
+# Creating VPC
+resource "aws_vpc" "wordpress-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+    tags = {
+      Name = "wordpress-vpc"
+    }
+}
+
+# Creating Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.wordpress-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a" 
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+# Creating route table
+resource "aws_route_table" "custom-route-table" {
+  vpc_id = aws_vpc.wordpress-vpc.id
+
+  tags = {
+    Name = "custom-route-table"
+  }
+}
+
+# Subnet Association with Route Table
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.custom-route-table.id
+}
+
+# Creating Internet Gateway and attach to subnet
+resource "aws_internet_gateway" "my-internet-gateway" {
+  vpc_id = aws_vpc.wordpress-vpc.id
+
+  tags = {
+    Name = "my-internet-gateway"
+  }
+}
+
+# Creating security Group
+resource "aws_security_group" "web-security-group" {
+  name        = "web-security-group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.wordpress-vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-security-group"
+  }
+}
+
+# Creating EC2 Instanc
+resource "aws_instance" "web_server" {
+  ami           = "ami-087f352c165340ea1"  # Amazon Linux 2023 in us-west-2
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = "vockey"  
+
+  vpc_security_group_ids = [
+    aws_security_group.web-security-group.id
+  ]
+
+  associate_public_ip_address = true
   user_data = <<-EOF
               #!/bin/bash
               apt update -y
@@ -111,6 +196,12 @@ resource "aws_instance" "web_server" {
 
               systemctl restart apache2
               EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
 
   tags = {
     Name = "web-server"
