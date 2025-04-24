4d28507b07756b9840091427a7ed8a829c6a349a
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-04-24 16:32:07 +0200

    cloud watch is not  possible due to sandbox restrictions

--- provider.tf
+++ provider.tf
@@ -361,47 +361,5 @@ resource "aws_db_instance" "wordpress_db" {
   }
 }
 
-# [Your existing resources... (VPC, Subnets, EC2 instances, RDS, etc.)]
-
-# IAM Role for CloudWatch Agent
-resource "aws_iam_role" "cloudwatch_agent_role" {
-  name = "cloudwatch-agent-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-# Attach CloudWatchAgentServerPolicy to the IAM Role
-resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy_attachment" {
-  role       = aws_iam_role.cloudwatch_agent_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
-
-# IAM Instance Profile for EC2 (Attach CloudWatch Role)
-resource "aws_iam_instance_profile" "cloudwatch_instance_profile" {
-  name = "cloudwatch-instance-profile"
-  role = aws_iam_role.cloudwatch_agent_role.name
-}
-
-# CloudWatch Log Group
-resource "aws_cloudwatch_log_group" "wordpress_log_group" {
-  name              = "/wordpress/logs"
-  retention_in_days = 7
-
-  tags = {
-    Name = "wordpress-log-group"
-  }
-}
-
 
 
