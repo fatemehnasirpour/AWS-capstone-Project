9c76c5bb41092863b5143e7a263f687b57ffde82
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-04-29 14:48:29 +0200

    Fix syntax: moved health_check inside aws_lb_target_group and updated path to /wp-login.php

--- 05.alb_and_asg.tf
+++ 05.alb_and_asg.tf
@@ -19,15 +19,15 @@ resource "aws_lb_target_group" "wordpress_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.wordpress-vpc.id
-
-  health_check {
-  path                = "/wp-login.php"
-  interval            = 30
-  timeout             = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 2
-  matcher             = "200-399"
 }
+  health_check {
+    path                = "/wp-login.php"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
 
 
 resource "aws_lb_listener" "wordpress_listener" {
