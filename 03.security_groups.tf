104b8bd1b93b6eb5fcae439177da16bd4424fd57
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-04-30 16:36:05 +0200

    Fixing securitz group name

--- 03.security_groups.tf
+++ 03.security_groups.tf
@@ -47,15 +47,16 @@ resource "aws_security_group" "alb_sg" {
 
   egress {
     description = "Allow outbound HTTP to EC2 SG"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    security_groups =  [aws_security_group.web-security-group.id] 
+     from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+   
   }
 
 
   tags = {
-    Name = "alb_sg"
+    Name = "alb-security-group"
   }
 }
 # RDS: DB Subnet Group
