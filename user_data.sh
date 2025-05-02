d6ca310f85317ba5d77b148ae17cb08b526b4597
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-05-02 18:25:47 +0200

    update user data

--- user_data.sh
+++ user_data.sh
@@ -1,41 +1,26 @@
 #!/bin/bash
-# Update system packages
 yum update -y
 
-# Add MariaDB 10.5 repository
-cat > /etc/yum.repos.d/MariaDB.repo <<EOF
-[mariadb]
-name = MariaDB
-baseurl = https://mirror.23media.com/mariadb/yum/10.5/centos7-amd64
-gpgkey=https://mirror.23media.com/mariadb/yum/RPM-GPG-KEY-MariaDB
-gpgcheck=1
-EOF
+# Install Apache and PHP
+amazon-linux-extras enable php7.4
+yum install -y httpd php php-mysqlnd php-fpm php-json php-mbstring mariadb105
 
-# Install Apache, PHP, MariaDB 10.5 server and client, unzip
-yum install -y httpd php php-mysqlnd MariaDB-server MariaDB-client wget unzip
-
-# Start and enable Apache and MariaDB
+# Start and enable Apache
 systemctl start httpd
 systemctl enable httpd
-systemctl start mariadb
-systemctl enable mariadb
 
-# Secure MariaDB (no interactive prompts)
-mysql -e "UPDATE mysql.user SET Password=PASSWORD('StrongRootPassword!') WHERE User='root';"
-mysql -e "DELETE FROM mysql.user WHERE User='';"
-mysql -e "DROP DATABASE IF EXISTS test;"
-mysql -e "FLUSH PRIVILEGES;"
+# Download and configure WordPress
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -r wordpress/* /var/www/html/
 
-# Download and install WordPress
-cd /var/www/html
-wget https://wordpress.org/latest.zip
-unzip latest.zip
-cp -r wordpress/* .
-rm -rf wordpress latest.zip
-
-# Set permissions
+# Set ownership and permissions
 chown -R apache:apache /var/www/html
 chmod -R 755 /var/www/html
 
-# Restart Apache
+# Create simple health check page
+echo "<h1>WordPress instance is alive</h1>" > /var/www/html/index.html
+
+# Restart Apache to load changes
 systemctl restart httpd
+
