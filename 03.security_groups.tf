abff297f65cb7577230253894e0761a09a4cd0bf
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-05-02 11:00:54 +0200

    Fix ALB 502 bad gateway issue

--- 03.security_groups.tf
+++ 03.security_groups.tf
@@ -12,13 +12,14 @@ resource "aws_security_group" "web-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ ingress {
+  description     = "HTTP from ALB SG"
+  from_port       = 80
+  to_port         = 80
+  protocol        = "tcp"
+  security_groups = [aws_security_group.alb_sg.id]
+}
+
 
   egress {
     from_port   = 0
