7353f318ffcefe5f9dca85195845361e15795c52
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-05-05 21:50:21 +0200

    update user data

--- 05.alb_and_asg.tf
+++ 05.alb_and_asg.tf
@@ -8,7 +8,7 @@ resource "aws_lb" "wordpress_alb" {
     aws_subnet.public_subnet_1.id,
     aws_subnet.public_subnet_2.id
   ]
-
+  enable_deletion_protection = false
   tags = {
     Name = "wordpress-alb"
   }
@@ -19,20 +19,18 @@ resource "aws_lb_target_group" "wordpress_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.wordpress-vpc.id 
- health_check {
-    path                = "/index.php"          
-    protocol            = "HTTP"       
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200-399"   
-  }
+ 
 
   target_type = "instance"            
 
  }
 
+resource "aws_lb_target_group_attachment" "wordpress-tg_attachment" {
+  target_group_arn = aws_lb_target_group.wordpress_tg.arn
+  
+  target_id        = aws_instance.wordpress_instance.id
+  port             = 80
+}
 resource "aws_lb_listener" "wordpress_listener" {
   load_balancer_arn = aws_lb.wordpress_alb.arn
   port              = 80
@@ -79,6 +77,8 @@ resource "aws_autoscaling_group" "wordpress_asg" {
   min_size             = 1
   vpc_zone_identifier  = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   target_group_arns    = [aws_lb_target_group.wordpress_tg.arn]
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
   launch_template {
     id      = aws_launch_template.wordpress_template.id
     version = "$Latest"
--- user_data.sh
+++ user_data.sh
@@ -1,26 +1,71 @@
-#!/bin/bash
-yum update -y
+#! /bin/bash
+# # Install updates
+sudo yum update -y
+sudo yum install -y stress-ng
 
-# Install Apache and PHP
-amazon-linux-extras enable php7.4
-yum install -y httpd php php-mysqlnd php-fpm php-json php-mbstring mariadb105
+#Install httpd
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
-# Start and enable Apache
-systemctl start httpd
-systemctl enable httpd
+#Install mysql
+sudo yum install -y mysql
 
-# Download and configure WordPress
-wget https://wordpress.org/latest.tar.gz
-tar -xzf latest.tar.gz
-cp -r wordpress/* /var/www/html/
+#Install PHP
+sudo yum install -y php 
+sudo amazon-linux-extras install 
 
-# Set ownership and permissions
-chown -R apache:apache /var/www/html
-chmod -R 755 /var/www/html
 
-# Create simple health check page
-echo "<h1>WordPress instance is alive</h1>" > /var/www/html/index.html
+# Update all installed 
+sudo yum update -y
 
-# Restart Apache to load changes
-systemctl restart httpd
+#Restart Apache
+sudo systemctl restart httpd
 
+#Install Wordpress
+DBRootPassword='rootpassword'
+mysqladmin -u root password $DBRootPassword
+
+# Retrieve RDS endpoint from Terraform output
+DBName=${rds_db_name}
+DBUser=${rds_username}
+DBPassword=${rds_password}
+RDS_ENDPOINT=${rds_endpoint}
+
+# Create a temporary file to store the database value
+sudo touch db.txt
+sudo chmod 777 db.txt
+sudo echo "DATABASE $DBName;" >> db.txt
+sudo echo "USER $DBUser;" >> db.txt
+sudo echo "PASSWORD $DBPassword;" >> db.txt
+sudo echo "HOST $RDS_ENDPOINT;" >> db.txt
+
+
+sudo yum install -y wget
+sudo wget http://wordpress.org/latest.tar.gz -P /var/www/html/
+
+cd /var/www/html
+sudo tar -zxvf latest.tar.gz
+sudo cp -rvf wordpress/* .
+
+sudo rm -R wordpress
+sudo rm latest.tar.gz
+
+echo "Terraform output:"
+
+# Copy wp-config.php file to wordpress directory
+sudo cp ./wp-config-sample.php ./wp-config.php
+sudo sed -i "s/'database_name_here'/'$DBName'/g" wp-config.php
+sudo sed -i "s/'username_here'/'$DBUser'/g" wp-config.php
+sudo sed -i "s/'password_here'/'$DBPassword'/g" wp-config.php
+sudo sed -i "s/'localhost'/'$RDS_ENDPOINT'/g" wp-config.php
+
+sudo mysql -h "$RDS_ENDPOINT" -u "$DBUser" -p"$DBPassword" "$DBName" -e "SHOW DATABASES;"
+
+#Install PHP Extensions
+sudo amazon-linux-extras enable php7.4
+sudo yum clean metadata
+sudo yum install -y php-cli php-pdo php-fpm php-json php-mysqlnd
+
+# Restart Apache
+sudo systemctl restart httpd
