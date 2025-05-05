a93a2b5c023e8dae0416823d2e13fcc89f0894ec
Fatemeh <FATEMEHNASIRPOUR68@GMAIL.COM>
2025-05-05 10:47:06 +0200

    disabl autoscaling userdata

--- 05.alb_and_asg.tf
+++ 05.alb_and_asg.tf
@@ -53,14 +53,9 @@ resource "aws_launch_template" "wordpress_template" {
   vpc_security_group_ids = [aws_security_group.web-security-group.id]
 
   # Use a dynamic variable in user_data to pass the RDS endpoint
-user_data = base64encode(templatefile("${path.module}/user_data.sh", {
-  rds_endpoint = aws_db_instance.wordpress_db.endpoint
-}))
-
-
-
-  
-
+ #user_data = base64encode(templatefile("${path.module}/user_data.sh", {
+  #rds_endpoint = aws_db_instance.wordpress_db.endpoint
+#}))
   tag_specifications {
     resource_type = "instance"
 
